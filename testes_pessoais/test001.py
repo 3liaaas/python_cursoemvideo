ca8ab504295d34a086bff0a06721ef25f0152376
3liax <contato.elias021021@gmail.com>
2025-09-03 20:50:12 -0300

    testes e desafio003

---
+++ testes_pessoais/test001.py
@@ -0,0 +1,5 @@
+n1 = int(input ('Digite um Número:'))
+n2 = int(input ('Digite um outro número:'))
+s = n1 + n2
+# print('a soma vale {}'.format(s))
+print ('A soma entre {0} e {1} vale {2}'.format(n1, n2, s))
