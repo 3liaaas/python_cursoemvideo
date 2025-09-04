8a8c5c73837bc4a6899f101011ec185af7d01cce
3liax <contato.elias021021@gmail.com>
2025-09-03 21:20:49 -0300

    deasafio 4

---
+++ desafios/desafio004.py
@@ -0,0 +1,7 @@
+n1 = input('Diga algo meu pç: ')
+
+print('o tipo primitivo desse valor é', type(n1))
+print('é alfabético?', n1.isalpha())
+print('é numérico?', n1.isnumeric())
+print('é decimal?', n1.isdecimal())
+print('só tem espaços?',n1.isspace())
