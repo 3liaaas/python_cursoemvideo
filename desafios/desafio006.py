57c45d1bad363cbee16e49dab739e4baeb047116
3liax <contato.elias021021@gmail.com>
2025-09-05 15:31:40 -0300

    desafios 006 e 007

---
+++ desafios/desafio006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+dob = n * 2
+trip = n * 3
+raiz = n ** (1/2)
+print('O dobro de {} é {}'.format(n, dob))
+print('O triplo de {} é {}'.format(n, trip))
+print('A raíz quadrada de {} é {}'.format(n, raiz))
---
+++ desafios/desafio007.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome: '))
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua primeira nota: '))
+m = (n1 + n2) / 2
+print('A nota média de {} é: {}'.format(nome, m))
