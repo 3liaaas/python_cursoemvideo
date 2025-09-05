ded2639b059c22c81c27e9dc90d736259727af25
3liax <contato.elias021021@gmail.com>
2025-09-05 15:19:39 -0300

    começo pycharm

---
+++ desafios/desafio005.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número inteiro:'))
+ant = n - 1
+suc = n + 1
+print('O número digitado foi: {}'.format(n))
+print('O número anterior a {} é {}'.format(n, ant))
+print('O número sucessor a {} é {}'.format(n, suc))
